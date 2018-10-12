d72ae1dc3b9bc25ca97ce6e2876e822b30ff296d
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-10-12 09:15:40 +0200

    Update Program.cs

--- AlgorytmyIStrukturyDanych/PS01/ASD1/ASD1/Program.cs
+++ AlgorytmyIStrukturyDanych/PS01/ASD1/ASD1/Program.cs
@@ -1,21 +1,30 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.IO;
-namespace ASD1
-{
+namespace ASD1{
 	class MainClass
     {
         public static void Main(string[] args)
         {
-			StreamReader input_file = new StreamReader("in.txt");
-			string[] lines = File.ReadAllLines("in.txt");
-			input_file.Close();
+			StreamWriter test = new StreamWriter("8.txt");
+			test.WriteLine(1000);
+			for (int o = 1; o <= 1000;o++)
+			{
+				test.WriteLine(o % 21);
+			}
+			test.Close();
+			string[] lines = File.ReadAllLines("8.txt");
 			int file_size = Int32.Parse(lines[0]);
             int[] input_array = new int[file_size];
+			int?[] tab = new int?[file_size]; //array used in second method
+		    long optimal_operations = 0;
+		    long naive_operations = 0;
 			for (int i = 1; i <= file_size;i++)
 			{
 				input_array[i - 1] = Int32.Parse(lines[i]);
+				tab[i-1]=Int32.Parse(lines[i]);
 			}
+
             /*
              * bool used to control while loop
              * it checks if any merging happened in current loop sesion
@@ -23,6 +32,7 @@ namespace ASD1
              * and algorithm should stop
             */
 			bool merge_in_current_loop = true;
+			bool need_to_expand_search = false;
 			int result_of_counting=0;
 			int quotient = 0;
 			int reminder = 0;
@@ -40,7 +50,7 @@ namespace ASD1
 			 * Each number a<=1000
 			 * Therefore sufficient size of counting array is
 			 * max value of input array*8128
-			 * 8128 because there are 2^13 divisions by 2 to get from 1000 to 1
+			 * 8128 because there are 2^13 divisions by 2 to get from 10000 to 1
 			 * In worst case scenario where every number is 1000
 			 * our counting array will have 1000*8128 cells
 			 * containing int(32b)=4B
@@ -52,14 +62,15 @@ namespace ASD1
 			for (int i = 0; i < input_array.Length; i++)
             {
 				counting_array[input_array[i]]++;
+			    optimal_operations++;
             }
 			while(merge_in_current_loop)
 			{
 				result_of_counting = 0;
 				merge_in_current_loop = false;
-				for (int i = 0; i < max_value_of_input_array*factor;i++)
+				for (int i = 0; i <= max_value_of_input_array*factor;i++)
 				{
-					
+				    optimal_operations++;
 					if (counting_array[i] == 1)
                         result_of_counting++;
 					quotient = counting_array[i] / 2;
@@ -69,6 +80,9 @@ namespace ASD1
 					if (quotient != 0)
 					{
 						merge_in_current_loop = true;
+						if (i >= 0.5*max_value_of_input_array * factor) //not sure if this work
+							need_to_expand_search = true;
+							
 					}
 				}
                 /* 
@@ -76,11 +90,51 @@ namespace ASD1
                  * because iterator is out of range
                  * There might be a problem if counting works great on big numbers tho
                 */
-				if (max_value_of_input_array * factor * 2 < counting_array.Length)
+				if (max_value_of_input_array * factor * 2 < counting_array.Length && need_to_expand_search)
 					factor *= 2;
 			}
 			StreamWriter output_file = new StreamWriter("out.txt");
 			output_file.WriteLine(result_of_counting);
+			Console.WriteLine(optimal_operations); //gdy dane sa powtarzalne
+			//Koniec rozwiazania optymalnego O=k*n gdzie k jest stałą powtarzalności
+            //Gorszy sort algorytmow O=cn^2
+			int k = 0;
+			bool found_anything_in_this_loop = true;
+			while(found_anything_in_this_loop)
+			{
+				
+				found_anything_in_this_loop = false;
+				for (int j = k; j < file_size; j++)
+                {
+				    naive_operations++;
+                    if (tab[j] == null)
+                        continue;
+                    if (tab[k] == tab[j]&&k!=j)
+                    {
+                        tab[k] *= 2;
+                        tab[j] = null;
+                        found_anything_in_this_loop = true;
+						j = 0;
+						k = 0;
+						continue;
+                    }
+                }
+				if(found_anything_in_this_loop==false&&k!=file_size-1)
+				{
+					k++;
+					found_anything_in_this_loop = true;
+				}  				
+			}
+			int counter_of_not_nulls_in_array = 0;
+			for (int l = 0; l < file_size;l++)
+			{
+			    naive_operations++;
+				//Console.WriteLine(tab[l]); 
+				if (tab[l] != null)
+					counter_of_not_nulls_in_array++;
+			}    
+		    Console.WriteLine(naive_operations);    //gdy dane sa unikatowe       
+			output_file.WriteLine(counter_of_not_nulls_in_array);
 			output_file.Close();
         }
     }
