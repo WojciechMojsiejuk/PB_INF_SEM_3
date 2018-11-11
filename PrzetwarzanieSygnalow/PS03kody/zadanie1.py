e31c351da5e1181d6bff8f316d8157b1297931a8
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-11-11 15:01:52 +0100

    Update zadanie1.py

--- PrzetwarzanieSygnalow/PS03kody/zadanie1.py
+++ PrzetwarzanieSygnalow/PS03kody/zadanie1.py
@@ -1,30 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def genereteSineWave(amplitude,frequency, phase, fs):
-    t=np.arange(0,1-1/fs,1/fs) #w jednej sekundzie jest fs probek
-    y=np.sin(2*np.pi*frequency*t+phase)
-    return t,y
-
-def genereteComplexSineWave(k,N,phase):
-    n = np.arange(0, N, 1)
-    omega=2*np.pi*k/N
-    y=np.exp(complex(n*omega)+phase)
+def genereteSineWave(N):                        #funkcja generująca sinusoidę
+    x = np.array(np.linspace(0, 2 * np.pi, N))  #przedział 1 okresu
+    y = np.sin(x)                               #funkcja sinusa
     return y
 
+
 if __name__ == "__main__":
-    #3.1
-    N=64
-    [time,func]=genereteSineWave(1,1,0,N)
-    fourier=np.fft.fft(func)
-    real=fourier.real
-    imaginary=fourier.imag
-    angle=np.angle(fourier)
-    modulus=np.abs(fourier)
-    #fig=plt.figure(tight_layout=True, y=1.08)
-    #fig.suptitle("Transformata Fouriera")
+    N=64                                        #ilość próbek
+    func=genereteSineWave(N)
+    fourier=2*np.fft.fft(func)/N                #transformata fouriera funkcji sinusoidalnej
+    real=fourier.real                           #część rzeczywista transformaty
+    imaginary=fourier.imag                      #część urojona transformaty
+    angle=np.angle(fourier)                     #kąt transformaty
+    modulus=np.abs(fourier)                     #moduł transformaty
     plt.subplot(3,2,1)
-    plt.plot(func.real)
+    plt.plot(np.real(func))
     plt.title("Część rzeczywista funkcji sinusoidalnej")
     plt.xlabel("Nr próbki")
     plt.ylabel("Amplituda")
@@ -36,39 +28,23 @@ if __name__ == "__main__":
     plt.plot(func.imag)
     plt.subplot(3,2,3)
     plt.title("Część rzeczywista Transformaty Fouriera")
-    plt.xlabel("Nr próbki")
+    plt.xlabel("Nr pasma częstotliwościowego")
     plt.ylabel("Amplituda")
     plt.stem(real)
     plt.subplot(3,2,4)
     plt.title("Część urojona Transformaty Fouriera")
-    plt.xlabel("Nr próbki")
+    plt.xlabel("Nr pasma częstotliwościowego")
     plt.ylabel("Amplituda")
     plt.stem(imaginary)
     plt.subplot(3,2,5)
     plt.title("Kąt Transformaty Fouriera")
-    plt.xlabel("Nr próbki")
-    plt.ylabel("Kąt[rad]")
-    plt.stem(angle)
+    plt.xlabel("Nr pasma częstotliwościowego")
+    plt.ylabel("Faza[pi*rad]")
+    plt.stem(angle/np.pi)
     plt.subplot(3,2,6)
     plt.title("Moduł z Transformaty Fouriera")
-    plt.xlabel("Nr próki")
+    plt.xlabel("Nr pasma częstotliwościowego")
     plt.ylabel("Magnituda")
     plt.stem(modulus)
     plt.tight_layout()
     plt.show()
-
-
-
-    #3.3
-    plt.title("Odwrotna Transformata Fouriera")
-    plt.xlabel("nr próbki")
-    plt.ylabel("amplituda")
-    plt.xlim(-0.01, 64.01)
-    inversefourier=np.fft.ifft(fourier)
-    plt.stem(inversefourier)
-    plt.show()
-
-    #numer/indeks pasma czestotliwosciowego
-    #czesc urojona zadania2
-    #modul magnituda
-    #pi radianow faza
