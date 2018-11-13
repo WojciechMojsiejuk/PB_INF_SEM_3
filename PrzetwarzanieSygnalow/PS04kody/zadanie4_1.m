6a6501718b94153a78060c4f8c8a170e8cbbc3a3
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-11-13 21:55:37 +0100

    Update zadanie4_1.m

--- PrzetwarzanieSygnalow/PS04kody/zadanie4_1.m
+++ PrzetwarzanieSygnalow/PS04kody/zadanie4_1.m
@@ -1,26 +1,43 @@
 fs = 8000;
 czas = 5;
 t = [0:1/fs:czas-1/fs];
+%wspolczynnik
+alfa1=0.999;
 %funkcja generująca szum gaussowski
 y1 = randn(czas*fs,1);
 y1 = transpose(y1);
 %obwiednia szumu gaussowskiego
-[up1,lo1] = envelope(y1);
+%zakladamy ze dla t<=0 gaussEnvelope zwraca 0
+gaussEnvelope(1)=(alfa1-1)*y1(1);
+for n = 2:fs*time
+  gaussEnvelope(n) = alfa1*gaussEnvelope(n-1)+(alfa1-1)*y1(n)^2;
+end
 %funkcja generująca sygnał sinusoidalny o stałej częstotliwości
 y2 = sin(250*pi*t);
 %obwiednia sygnału sinusoidalnego
-[up2,lo2] = envelope(y2);
+sinEnvelope(1)=(alfa1-1)*y2(1);
+for n = 2:fs*time
+  sinEnvelope(n) = alfa1*sinEnvelope(n-1)+(alfa1-1)*y2(n)^2;
+end
 %funkcja generująca sygnał o zmiennej częstotliwości w zakresie od 0Hz (0s)
 %do 1kHz(5s)
-y3 = chirp(t,0,5,1000,'quadratic',[],'concave');
+y3 = chirp(t,0,5,1000);
 %obwiednia sygnału zmiennego
-[up3,lo3] = envelope(y3);
+chirpEnvelope(1)=(alfa1-1)*y3(1);
+for n = 2:fs*time
+  chirpEnvelope(n) = alfa1*chirpEnvelope(n-1)+(alfa1-1)*y3(n)^2;
+end
 %wczytanie sygnału mowy
 [y4,fs]=audioread('mowa.wav');
 %obwiednia sygnału mowy;
-[up4,lo4] = envelope(y4);
+voiceEnvelope(1)=(alfa1-1)*y4(1);
+for n = 2:fs*time
+  voiceEnvelope(n) = alfa1*voiceEnvelope(n-1)+(alfa1-1)*y4(n)^2;
+end
+
 figure;
 %SZUM GAUSSOWSKI
+
 subplot(4,2,1);
 hold on;
 plot(t,y1);
@@ -31,14 +48,11 @@ hold off;
 subplot(4,2,2);
 hold on;
 title('Szum gaussowski - Obwiednia mocy, \alpha = 0.999')
-alpha=0.999;
-%pierwsza wartość nie może odwołać się do poprzednich
-up1(1)=(1-alpha)*up1(1)^2;
-for n = 2:fs*czas
-    up1(n)=alpha*up1(n-1)+(1-alpha)*up1(n)^2;
-end
-plot(t,up1);
+axis([-inf inf 0 2]);
+%Z jakiegos powodu nalezy pomnozyc przez -1
+plot(t,(-1)*gaussEnvelope);
 hold off;
+
 %SYGNAŁ SINUSOIDALNY STAŁY
 subplot(4,2,3)
 hold on;
@@ -50,19 +64,10 @@ hold off;
 subplot(4,2,4)
 hold on;
 title('Sygnał sinusoidalny - Obwiednia mocy, \alpha = 0.999')
-alpha=0.999;
-%pierwsza wartość nie może odwołać się do poprzednich
-up2(1)=(1-alpha)*up2(1)^2;
-for n = 2:fs*czas
-    up2(n)=alpha*up2(n-1)+(1-alpha)*up2(n)^2;
-end
-lo2(1)=(1-alpha)*lo2(1)^2;
-for n = 2:fs*czas
-    lo2(n)=alpha*lo2(n-1)+(1-alpha)*lo2(n)^2;
-end
-%lo rysuje tak samo up by było jak w przykladowym podzielimy przez 2
-plot(t,lo2);
+axis([-inf inf 0 1]);
+plot(t,(-1)*sinEnvelope);
 hold off;
+
 %SYGNAŁ ZMIENNY
 subplot(4,2,5)
 hold on;
@@ -74,18 +79,10 @@ hold off;
 subplot(4,2,6)
 hold on;
 title('Sygnał o zmiennej częstotliwości - Obwiednia mocy, \alpha = 0.999')
-alpha=0.999;
-%pierwsza wartość nie może odwołać się do poprzednich
-up3(1)=(1-alpha)*up3(1)^2;
-for n = 2:fs*czas
-    up3(n)=alpha*up3(n-1)+(1-alpha)*up3(n)^2;
-end
-lo3(1)=(1-alpha)*lo3(1)^2;
-for n = 2:fs*czas
-    lo3(n)=alpha*lo3(n-1)+(1-alpha)*lo3(n)^2;
-end
-plot(t,up3,t,lo3);
+axis([-inf inf 0 1]);
+plot(t,(-1)*chirpEnvelope);
 hold off;
+
 %SYGNAŁ MOWY
 subplot(4,2,7)
 hold on;
@@ -97,28 +94,49 @@ hold off;
 subplot(4,2,8)
 hold on;
 title('Sygnał mowy - Obwiednia mocy, \alpha = 0.999')
-alpha=0.999;
-%pierwsza wartość nie może odwołać się do poprzednich
-up4(1)=(1-alpha)*up4(1)^2;
-for n = 2:fs*czas
-    up4(n)=alpha*up4(n-1)+(1-alpha)*up4(n)^2;
-end
-lo4(1)=(1-alpha)*lo4(1)^2;
-for n = 2:fs*czas
-    lo4(n)=alpha*lo4(n-1)+(1-alpha)*lo4(n)^2;
-end
-plot(t,up4,t,lo4);
+axis([-inf inf 0 0.1]);
+plot(t,-1*voiceEnvelope);
 hold off;
-
-
 
 %POWTÓRZENIE WYKRESÓW DLA ALFA=0.99
 
-
-
+%wspolczynnik
+alfa1=0.99;
+%funkcja generująca szum gaussowski
+y1 = randn(czas*fs,1);
+y1 = transpose(y1);
+%obwiednia szumu gaussowskiego
+%zakladamy ze dla t<=0 gaussEnvelope zwraca 0
+gaussEnvelope(1)=(alfa1-1)*y1(1);
+for n = 2:fs*time
+  gaussEnvelope(n) = alfa1*gaussEnvelope(n-1)+(alfa1-1)*y1(n)^2;
+end
+%funkcja generująca sygnał sinusoidalny o stałej częstotliwości
+y2 = sin(250*pi*t);
+%obwiednia sygnału sinusoidalnego
+sinEnvelope(1)=(alfa1-1)*y2(1);
+for n = 2:fs*time
+  sinEnvelope(n) = alfa1*sinEnvelope(n-1)+(alfa1-1)*y2(n)^2;
+end
+%funkcja generująca sygnał o zmiennej częstotliwości w zakresie od 0Hz (0s)
+%do 1kHz(5s)
+y3 = chirp(t,0,5,1000);
+%obwiednia sygnału zmiennego
+chirpEnvelope(1)=(alfa1-1)*y3(1);
+for n = 2:fs*time
+  chirpEnvelope(n) = alfa1*chirpEnvelope(n-1)+(alfa1-1)*y3(n)^2;
+end
+%wczytanie sygnału mowy
+[y4,fs]=audioread('mowa.wav');
+%obwiednia sygnału mowy;
+voiceEnvelope(1)=(alfa1-1)*y4(1);
+for n = 2:fs*time
+  voiceEnvelope(n) = alfa1*voiceEnvelope(n-1)+(alfa1-1)*y4(n)^2;
+end
 
 figure;
 %SZUM GAUSSOWSKI
+
 subplot(4,2,1);
 hold on;
 plot(t,y1);
@@ -129,14 +147,11 @@ hold off;
 subplot(4,2,2);
 hold on;
 title('Szum gaussowski - Obwiednia mocy, \alpha = 0.99')
-alpha=0.99;
-%pierwsza wartość nie może odwołać się do poprzednich
-up1(1)=(1-alpha)*up1(1)^2;
-for n = 2:fs*czas
-    up1(n)=alpha*up1(n-1)+(1-alpha)*up1(n)^2;
-end
-plot(t,up1);
+axis([-inf inf 0 2]);
+%Z jakiegos powodu nalezy pomnozyc przez -1
+plot(t,(-1)*gaussEnvelope);
 hold off;
+
 %SYGNAŁ SINUSOIDALNY STAŁY
 subplot(4,2,3)
 hold on;
@@ -148,19 +163,10 @@ hold off;
 subplot(4,2,4)
 hold on;
 title('Sygnał sinusoidalny - Obwiednia mocy, \alpha = 0.99')
-alpha=0.99;
-%pierwsza wartość nie może odwołać się do poprzednich
-up2(1)=(1-alpha)*up2(1)^2;
-for n = 2:fs*czas
-    up2(n)=alpha*up2(n-1)+(1-alpha)*up2(n)^2;
-end
-lo2(1)=(1-alpha)*lo2(1)^2;
-for n = 2:fs*czas
-    lo2(n)=alpha*lo2(n-1)+(1-alpha)*lo2(n)^2;
-end
-%lo rysuje tak samo up by było jak w przykladowym podzielimy przez 2
-plot(t,lo2);
+axis([-inf inf 0 1]);
+plot(t,(-1)*sinEnvelope);
 hold off;
+
 %SYGNAŁ ZMIENNY
 subplot(4,2,5)
 hold on;
@@ -172,18 +178,10 @@ hold off;
 subplot(4,2,6)
 hold on;
 title('Sygnał o zmiennej częstotliwości - Obwiednia mocy, \alpha = 0.99')
-alpha=0.99;
-%pierwsza wartość nie może odwołać się do poprzednich
-up3(1)=(1-alpha)*up3(1)^2;
-for n = 2:fs*czas
-    up3(n)=alpha*up3(n-1)+(1-alpha)*up3(n)^2;
-end
-lo3(1)=(1-alpha)*lo3(1)^2;
-for n = 2:fs*czas
-    lo3(n)=alpha*lo3(n-1)+(1-alpha)*lo3(n)^2;
-end
-plot(t,up3,t,lo3);
+axis([-inf inf 0 1]);
+plot(t,(-1)*chirpEnvelope);
 hold off;
+
 %SYGNAŁ MOWY
 subplot(4,2,7)
 hold on;
@@ -195,26 +193,49 @@ hold off;
 subplot(4,2,8)
 hold on;
 title('Sygnał mowy - Obwiednia mocy, \alpha = 0.99')
-alpha=0.99;
-%pierwsza wartość nie może odwołać się do poprzednich
-up4(1)=(1-alpha)*up4(1)^2;
-for n = 2:fs*czas
-    up4(n)=alpha*up4(n-1)+(1-alpha)*up4(n)^2;
-end
-lo4(1)=(1-alpha)*lo4(1)^2;
-for n = 2:fs*czas
-    lo4(n)=alpha*lo4(n-1)+(1-alpha)*lo4(n)^2;
-end
-plot(t,up4,t,lo4);
+axis([-inf inf 0 0.2]);
+plot(t,-1*voiceEnvelope);
 hold off;
 
-%POWTÓRZENIE WYKRESÓW DLA ALFA=0.01
+%POWTÓRZENIE WYKRESÓW DLA ALFA=0.001
 
-
-
+%wspolczynnik
+alfa1=0.001;
+%funkcja generująca szum gaussowski
+y1 = randn(czas*fs,1);
+y1 = transpose(y1);
+%obwiednia szumu gaussowskiego
+%zakladamy ze dla t<=0 gaussEnvelope zwraca 0
+gaussEnvelope(1)=(alfa1-1)*y1(1);
+for n = 2:fs*time
+  gaussEnvelope(n) = alfa1*gaussEnvelope(n-1)+(alfa1-1)*y1(n)^2;
+end
+%funkcja generująca sygnał sinusoidalny o stałej częstotliwości
+y2 = sin(250*pi*t);
+%obwiednia sygnału sinusoidalnego
+sinEnvelope(1)=(alfa1-1)*y2(1);
+for n = 2:fs*time
+  sinEnvelope(n) = alfa1*sinEnvelope(n-1)+(alfa1-1)*y2(n)^2;
+end
+%funkcja generująca sygnał o zmiennej częstotliwości w zakresie od 0Hz (0s)
+%do 1kHz(5s)
+y3 = chirp(t,0,5,1000);
+%obwiednia sygnału zmiennego
+chirpEnvelope(1)=(alfa1-1)*y3(1);
+for n = 2:fs*time
+  chirpEnvelope(n) = alfa1*chirpEnvelope(n-1)+(alfa1-1)*y3(n)^2;
+end
+%wczytanie sygnału mowy
+[y4,fs]=audioread('mowa.wav');
+%obwiednia sygnału mowy;
+voiceEnvelope(1)=(alfa1-1)*y4(1);
+for n = 2:fs*time
+  voiceEnvelope(n) = alfa1*voiceEnvelope(n-1)+(alfa1-1)*y4(n)^2;
+end
 
 figure;
 %SZUM GAUSSOWSKI
+
 subplot(4,2,1);
 hold on;
 plot(t,y1);
@@ -225,14 +246,11 @@ hold off;
 subplot(4,2,2);
 hold on;
 title('Szum gaussowski - Obwiednia mocy, \alpha = 0.001')
-alpha=0.001;
-%pierwsza wartość nie może odwołać się do poprzednich
-up1(1)=(1-alpha)*up1(1)^2;
-for n = 2:fs*czas
-    up1(n)=alpha*up1(n-1)+(1-alpha)*up1(n)^2;
-end
-plot(t,up1);
+axis([-inf inf 0 20]);
+%Z jakiegos powodu nalezy pomnozyc przez -1
+plot(t,(-1)*gaussEnvelope);
 hold off;
+
 %SYGNAŁ SINUSOIDALNY STAŁY
 subplot(4,2,3)
 hold on;
@@ -244,19 +262,10 @@ hold off;
 subplot(4,2,4)
 hold on;
 title('Sygnał sinusoidalny - Obwiednia mocy, \alpha = 0.001')
-alpha=0.001;
-%pierwsza wartość nie może odwołać się do poprzednich
-up2(1)=(1-alpha)*up2(1)^2;
-for n = 2:fs*czas
-    up2(n)=alpha*up2(n-1)+(1-alpha)*up2(n)^2;
-end
-lo2(1)=(1-alpha)*lo2(1)^2;
-for n = 2:fs*czas
-    lo2(n)=alpha*lo2(n-1)+(1-alpha)*lo2(n)^2;
-end
-%lo rysuje tak samo up by było jak w przykladowym podzielimy przez 2
-plot(t,lo2);
+axis([-inf inf 0 1]);
+plot(t,(-1)*sinEnvelope);
 hold off;
+
 %SYGNAŁ ZMIENNY
 subplot(4,2,5)
 hold on;
@@ -268,18 +277,10 @@ hold off;
 subplot(4,2,6)
 hold on;
 title('Sygnał o zmiennej częstotliwości - Obwiednia mocy, \alpha = 0.001')
-alpha=0.001;
-%pierwsza wartość nie może odwołać się do poprzednich
-up3(1)=(1-alpha)*up3(1)^2;
-for n = 2:fs*czas
-    up3(n)=alpha*up3(n-1)+(1-alpha)*up3(n)^2;
-end
-lo3(1)=(1-alpha)*lo3(1)^2;
-for n = 2:fs*czas
-    lo3(n)=alpha*lo3(n-1)+(1-alpha)*lo3(n)^2;
-end
-plot(t,up3,t,lo3);
+axis([-inf inf 0 1]);
+plot(t,(-1)*chirpEnvelope);
 hold off;
+
 %SYGNAŁ MOWY
 subplot(4,2,7)
 hold on;
@@ -291,15 +292,7 @@ hold off;
 subplot(4,2,8)
 hold on;
 title('Sygnał mowy - Obwiednia mocy, \alpha = 0.001')
-alpha=0.001;
-%pierwsza wartość nie może odwołać się do poprzednich
-up4(1)=(1-alpha)*up4(1)^2;
-for n = 2:fs*czas
-    up4(n)=alpha*up4(n-1)+(1-alpha)*up4(n)^2;
-end
-lo4(1)=(1-alpha)*lo4(1)^2;
-for n = 2:fs*czas
-    lo4(n)=alpha*lo4(n-1)+(1-alpha)*lo4(n)^2;
-end
-plot(t,up4,t,lo4);
+axis([-inf inf 0 1]);
+plot(t,-1*voiceEnvelope);
 hold off;
+
