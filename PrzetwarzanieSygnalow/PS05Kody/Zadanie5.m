3b0ff3d9ff646f48dd63553144b25681e4684dc7
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-11-22 12:42:06 +0100

    Update Zadanie5.m

--- PrzetwarzanieSygnalow/PS05Kody/Zadanie5.m
+++ PrzetwarzanieSygnalow/PS05Kody/Zadanie5.m
@@ -1,24 +1,24 @@
 %nagranie bezechowe
 
-[y,Fs] = audioread('Audio.wav')
+[y,Fs] = audioread('Audio.wav');
 t=0:1/Fs:5-1/Fs;
 signal = y(1:8*Fs,1)';
 %stem(t,signal)
 %sound(signal,Fs);
 
 %odpowiedz impulsowa Katedra
-[iy,iFs]= audioread('CathedralRoom.wav')
-impulse = 1/100*iy(:,1)';
+[iy,iFs]= audioread('CathedralRoom.wav');
+impulse = iy(:,1)';
 %sound(impulse);
 wynik = conv(signal,impulse,'same');
 stem(wynik);
-%sound(wynik,Fs);
+sound(1/100*wynik,Fs);
 
 
 %odpowiedz impulsowa jaskinia
-[i2y,i2Fs]= audioread('ir_2_L_2.wav')
-impulse2 = 1/100*i2y(:,1)';
+[i2y,i2Fs]= audioread('BatteryBenson.wav');
+impulse2 = i2y(:,1)';
 %sound(impulse);
 wynik2 = conv(signal,impulse2,'same');
 stem(wynik2);
-sound(wynik2,i2Fs);
+sound(1/100*wynik2,i2Fs);
